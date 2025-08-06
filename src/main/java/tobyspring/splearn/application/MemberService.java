32bb4e0ab451ba41ad017970acd56e30926bb128
SeungSoo <seung_soos@naver.com>
2025-08-06 21:52:44 +0900

    - 회원 애플리케이션 서비스 테스트

--- src/main/java/tobyspring/splearn/application/MemberService.java
+++ src/main/java/tobyspring/splearn/application/MemberService.java
@@ -5,9 +5,9 @@ import org.springframework.stereotype.Service;
 import tobyspring.splearn.application.required.EmailSender;
 import tobyspring.splearn.application.provided.MemberRegister;
 import tobyspring.splearn.application.required.MemberRepository;
-import tobyspring.splearn.domain.Member;
-import tobyspring.splearn.domain.MemberRegisterRequest;
-import tobyspring.splearn.domain.PasswordEncoder;
+import tobyspring.splearn.domain.*;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -19,9 +19,23 @@ public class MemberService implements MemberRegister {
 
     @Override
     public Member register(MemberRegisterRequest registerRequest) {
+        checkDuplicateEmail(registerRequest);
+
         Member member = Member.register(registerRequest, passwordEncoder);
+
         memberRepository.save(member);
-        emailSender.send(member.getEmail(), "등록을 완료해주세요", "링크 클릭");
+
+        sendWelcomeEmail(member);
         return member;
+    }
+
+    private void sendWelcomeEmail(Member member) {
+        emailSender.send(member.getEmail(), "등록을 완료해주세요", "링크 클릭");
+    }
+
+    private void checkDuplicateEmail(MemberRegisterRequest registerRequest) {
+        if (memberRepository.findByEmail(new Email(registerRequest.email())).isPresent()) {
+            throw new DuplicateEmailException("이미 사용중인 이메일입니다. " + registerRequest.email());
+        }
     }
 }
--- src/main/java/tobyspring/splearn/application/required/MemberRepository.java
+++ src/main/java/tobyspring/splearn/application/required/MemberRepository.java
@@ -1,9 +1,14 @@
 package tobyspring.splearn.application.required;
 
 import org.springframework.data.repository.Repository;
+import tobyspring.splearn.domain.Email;
 import tobyspring.splearn.domain.Member;
+
+import java.util.Optional;
 
 public interface MemberRepository extends Repository<Member, Long> {
 
     Member save(Member member);
+
+    Optional<Member> findByEmail(Email email);
 }
